f905e6d65d372ade52b4749781f9e55489a34b6d
Alex Satrapa <grail@goldweb.com.au>
2015-03-08 09:09:00 +1100

    Removing old code.

--- ds3234.h
+++ ds3234.h
@@ -43,42 +43,6 @@ struct ts {
     uint8_t year_s;      /* year in short notation*/
 };
 
-void DS3234_init(const uint8_t pin, const uint8_t creg);
-void DS3234_set(const uint8_t pin, struct ts t);
-void DS3234_get(const uint8_t pin, struct ts *t);
-
-void DS3234_set_addr(const uint8_t pin, const uint8_t addr, const uint8_t val);
-uint8_t DS3234_get_addr(const uint8_t pin, const uint8_t addr);
-
-// control/status register
-void DS3234_set_creg(const uint8_t pin, const uint8_t val);
-void DS3234_set_sreg(const uint8_t pin, const uint8_t mask);
-uint8_t DS3234_get_sreg(const uint8_t pin);
-
-// aging offset register
-void DS3234_set_aging(const uint8_t pin, const int8_t value);
-int8_t DS3234_get_aging(const uint8_t pin);
-
-// temperature register
-float DS3234_get_treg(const uint8_t pin);
-
-// alarms
-void DS3234_set_a1(const uint8_t pin, const uint8_t s, const uint8_t mi, const uint8_t h, const uint8_t d,
-                   const uint8_t * flags);
-void DS3234_get_a1(const uint8_t pin, char *buf, const uint8_t len);
-void DS3234_clear_a1f(const uint8_t pin);
-uint8_t DS3234_triggered_a1(const uint8_t pin);
-
-void DS3234_set_a2(const uint8_t pin, const uint8_t mi, const uint8_t h, const uint8_t d,
-                   const uint8_t * flags);
-void DS3234_get_a2(const uint8_t pin, char *buf, const uint8_t len);
-void DS3234_clear_a2f(const uint8_t pin);
-uint8_t DS3234_triggered_a2(const uint8_t pin);
-
-// sram
-void DS3234_set_sram_8b(const uint8_t pin, const uint8_t address, const uint8_t value);
-uint8_t DS3234_get_sram_8b(const uint8_t pin, const uint8_t address);
-
 // helpers
 uint8_t dectobcd(const uint8_t val);
 uint8_t bcdtodec(const uint8_t val);
