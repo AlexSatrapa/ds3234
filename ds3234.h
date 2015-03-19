fa9821532864a9495ef6c447a9c49a1c1538f845
Alex Satrapa <grail@goldweb.com.au>
2015-03-19 22:48:27 +1100

    Convert to subclass of DSRTC.

--- ds3234.h
+++ ds3234.h
@@ -17,57 +17,20 @@
 uint8_t dectobcd(const uint8_t val);
 uint8_t bcdtodec(const uint8_t val);
 
-class DS3234RTC
+class DS3234RTC: public DSRTC
 {
   public:
     DS3234RTC( uint8_t pin );
     DS3234RTC( uint8_t pin, const uint8_t ctrl_reg );
-    static bool available();
-    // Date and Time
-    time_t get();
-    static void set(time_t t);
-    void read(tmElements_t &tm);
-    void write(tmElements_t &tm);
-    void writeTime(tmElements_t &tm);
-    void writeDate(tmElements_t &tm);
-    // Alarms
-    void readAlarm(uint8_t alarm, alarmMode_t &mode, tmElements_t &tm);
-    void writeAlarm(uint8_t alarm, alarmMode_t mode, tmElements_t tm);
-    // Control Register
-    void setBBOscillator(bool enable);
-    void setBBSqareWave(bool enable);
-    void setSQIMode(sqiMode_t mode);
-    bool isAlarmInterrupt(uint8_t alarm);
-    uint8_t readControlRegister();
-    void writeControlRegister(uint8_t value);
-    // Status Register
-    bool isOscillatorStopFlag();
-    void setOscillatorStopFlag(bool enable);
-    void setBB33kHzOutput(bool enable);
-    void setTCXORate(tempScanRate_t rate);
-    void set33kHzOutput(bool enable);
-    bool isTCXOBusy();
-    bool isAlarmFlag(uint8_t alarm);
-    uint8_t isAlarmFlag();
-    void clearAlarmFlag(uint8_t alarm);
-    uint8_t readStatusRegister();
-    void writeStatusRegister(uint8_t value);
-    // Temperature
-    void readTemperature(tpElements_t &tmp);
     SPISettings spi_settings;
   private:
-    uint8_t dec2bcd(uint8_t dec);
-    uint8_t bcd2dec(uint8_t bcd);
     uint8_t ss_pin;
   protected:
     uint8_t read1(uint8_t addr);
     void write1(uint8_t addr, uint8_t data);
     void readN(uint8_t addr, uint8_t buf[], uint8_t len);
     void writeN(uint8_t addr, uint8_t buf[], uint8_t len);
-    void populateTimeElements( tmElements_t &tm, uint8_t TimeDate[] );
-    void populateDateElements( tmElements_t &tm, uint8_t TimeDate[] );
 };
-
 
 #endif
 // -*- coding: utf-8; tab-width: 4; indent-tabs-mode: nil -*-
